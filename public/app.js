9709ce10b1572ca8d601ab4cd8da36d148113c4b
Liam Cavens <liamcavens@googlemail.com>
2018-06-16 23:01:41 +0100

    cleaned up and added css

--- public/app.js
+++ public/app.js
@@ -1,5 +1,7 @@
 const app = function () {
-    getData('Glasgow')
+    getDataByCity('Glasgow')
+    //getCurrentLocation();
+
 
     const search = document.querySelector('#search-button');
     search.addEventListener("click", handleOptionSearch);
@@ -14,19 +16,31 @@ const makeRequest = function (url, callbackFunction) {
 
 const handleOptionSearch = function () {
     const city = document.querySelector('#city-search');
-    getData(city.value);
+    getDataByCity(city.value);
 };
 
-const getData = function (cityName) {
-    const url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=c4f320d668e5ec15a88bf95db5d697b1&q=${cityName}`;
+const getData = function (url) {
     makeRequest(url, requestComplete);
 };
 
+const getDataByCity = function (cityName) {
+    const url = `http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=c4f320d668e5ec15a88bf95db5d697b1&q=${cityName}`;
+
+    getData(url);
+};
+
+const getDataByCoords = function(lat, lon) {
+    const url = `http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=c4f320d668e5ec15a88bf95db5d697b1&qlat=${lat}&lon=${lon}`;
+
+    getData(url);
+}
+
 const requestComplete = function () {
     if (this.status !== 200) return;
-
+    
     const weatherData = JSON.parse(this.response);
-    populate(weatherData);
+    //console.log(weatherData);
+    populate(weatherData); 
 }
 
 const populate = function (data) {
@@ -34,12 +48,10 @@ const populate = function (data) {
     const cityHeader = document.querySelector('#city-name');
     const cityTemp = document.querySelector('#city-temp');
     const weatherType = document.querySelector('#weather-type');
-    const weatherIcon = document.querySelector('#weather-icon');
 
-    cityHeader.textContent = `${data.city.name}`;
-    cityTemp.innerHTML = `${(data.list[0].main.temp - 273).toFixed()}&#176;C`;
-    weatherType.textContent = `${data.list[0].weather[0].description}`;
-    weatherIcon.innerHTML = `<img src='http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png' alt="weather_icon">`;
+    cityHeader.textContent = `Weather in ${data.city.name}, ${data.city.country}`;
+    cityTemp.innerHTML = `Temperature : ${(data.list[0].main.temp - 273).toFixed()}&#176;C`;
+    weatherType.innerHTML = `Description : ${data.list[0].weather[0].description} <img id="weather-icon" src='http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png' alt="weather_icon">`;
 };
 
 const getCurrentLocation = function () {
@@ -47,11 +59,13 @@ const getCurrentLocation = function () {
 }
 
 const getCurrentLocationSuccess = function (position) {
-
+    const coords = position.coords;
+    
+    getDataByCoords(coords.latitude.toFixed(), coords.longitude.toFixed());
 }
 
 const getCurrentLocationFailure = function () {
-
+    getDataByCity('Glasgow');
 }
 
 window.addEventListener('load', app);
